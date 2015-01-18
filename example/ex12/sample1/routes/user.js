15ab442810b0f830f88fbb67b387e312546b9fb4
Koding <tkb2153@gmail.com>
2015-01-18 05:53:04 +0000

    [Temp]ex12 of sample1

---
+++ example/ex12/sample1/routes/user.js
@@ -0,0 +1,36 @@
+exports.list = function(req, res) {
+	res.send('respond with a resource');
+};
+
+exports.register = function(req, res) {
+	res.send('This is the register page.');
+}
+
+exports.signin = function(req, res) {
+	res.send('This is the signin page.');
+}
+
+exports.signout = function(req, res) {
+	res.send('This is the signout page.');
+}
+
+exports.forget = function(req, res) {
+	res.send('This is the forget page.');
+}
+
+exports.profile = function(req, res) {
+	res.send('This is the profile page.');
+}
+
+exports.article = function(req, res) {
+	res.send('This is the add_article page.');
+}
+
+exports.modify = function(req, res) {
+	res.send('This is the modify page.');
+}
+
+exports.message = function(req, res) {
+	res.send('This is the message page.');
+}
+
