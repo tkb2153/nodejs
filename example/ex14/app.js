83769b938240a822a790cbeb097f6e840dd5ab03
Nick <tkb2153@gmail.com>
2015-02-09 22:13:51 +0800

    [Temp] ex14

--- example/ex14/app.js
+++ example/ex14/app.js
@@ -4,14 +4,19 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  //res.send('<h1>Hello world</h1>');
 	res.sendfile(__dirname + '/index.html');
 });
 
+app.get('*', function(req, res) {
+	res.sendfile(__dirname + '/error.html');
+});
+
 io.on('connection', function(socket){
+	console.log(app.path());
 	console.log('a user connected');
 	socket.on('chat message', function(msg){
-		console.log('message: ' + msg);
+		//console.log('message: ' + msg);
+		io.emit('chat message', msg);
 	});
 });
 
